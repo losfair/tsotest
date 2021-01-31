0c1c9ebe01fd43ef449ddc20392a2eedd4e00140
losfair <zhy20000919@hotmail.com>
2021-01-31 10:40:32 +0000

    Fix it

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,67 @@
-use std::sync::atomic::{AtomicBool, Ordering};
+use std::ptr::{read_volatile, write_volatile};
+use std::sync::mpsc::{SyncSender, Receiver, channel, sync_channel};
 
-static B0: AtomicBool = AtomicBool::new(false);
-static B1: AtomicBool = AtomicBool::new(false);
-static B2: AtomicBool = AtomicBool::new(false);
+static mut V0: u64 = 0;
+static mut V1: u64 = 0;
+static mut V2: u64 = 0;
 
-fn worker() {
+fn th1(ch: SyncSender<()>, begin: Receiver<()>) {
+    enable_tso_thread();
     loop {
-        B0.store(true, Ordering::Relaxed);
-        B1.store(true, Ordering::Relaxed);
-        B2.store(true, Ordering::Relaxed);
-        while B2.load(Ordering::Relaxed) == true {}
-        B1.store(false, Ordering::Relaxed);
-        B0.store(false, Ordering::Relaxed);
+        begin.recv().unwrap();
+        unsafe {
+            write_volatile(&mut V0, 1);
+            write_volatile(&mut V1, 1);
+        }
+        ch.send(()).unwrap();
+    }
+}
+
+fn th2(ch: SyncSender<()>, begin: Receiver<()>) {
+    enable_tso_thread();
+    loop {
+        begin.recv().unwrap();
+        unsafe {
+            while read_volatile(&V1) == 0 {}
+            if read_volatile(&V0) != 0 {
+                write_volatile(&mut V2, 1);
+            }
+        }
+        ch.send(()).unwrap();
+    }
+}
+
+fn enable_tso_thread() {
+    #[cfg(feature = "apple-tso")]
+    {
+        use sysctl::Sysctl;
+        sysctl::Ctl::new("kern.tso_enable").unwrap().set_value_string("1").unwrap();
     }
 }
 
 fn main() {
-    std::thread::spawn(worker);
-    for i in 0..1000000000usize {
-        while B1.load(Ordering::Relaxed) == false {}
-        if B0.load(Ordering::Relaxed) != true {
-            panic!("TSO check failed at iteration {}", i);
+    enable_tso_thread();
+    let (begin1_tx, begin1_rx) = channel();
+    let (begin2_tx, begin2_rx) = channel();
+    let (tx1, rx1) = sync_channel(0);
+    let (tx2, rx2) = sync_channel(0);
+    std::thread::spawn(|| th1(tx1, begin1_rx));
+    std::thread::spawn(|| th2(tx2, begin2_rx));
+    for i in 0..10000000usize {
+        begin2_tx.send(()).unwrap();
+        begin1_tx.send(()).unwrap();
+        rx2.recv().unwrap();
+        rx1.recv().unwrap();
+        unsafe {
+            if read_volatile(&V2) != 1 {
+                panic!("TSO test failed at iteration {}", i);
+            }
+            write_volatile(&mut V0, 0);
+            write_volatile(&mut V1, 0);
+            write_volatile(&mut V2, 0);
         }
-        B2.store(false, Ordering::Relaxed);
+        if i % 500000 == 0 {
+            println!("progress: {}", i);
+        }
     }
 }
